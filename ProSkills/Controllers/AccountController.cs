df84b68f9395b8cecdddeef717b80d9db3eac68a
Muhammed Ahmed <sadaatvip@gmail.com>
2024-05-04 22:23:13 +0300

    Sadat - Admin page / test

--- ProSkills/Controllers/AccountController.cs
+++ ProSkills/Controllers/AccountController.cs
@@ -6,24 +6,24 @@ namespace ProSkills.Controllers
     public class AccountController : Controller
     {
        
-        public IActionResult Index()
-        {
-            return View();
-        }
+        //public IActionResult Index()
+        //{
+        //    return View();
+        //}
 
 
-        [HttpGet]
-        public IActionResult Register()
-        {
-            return View(Register);
-        }
-        [HttpPost]
-        public IActionResult Register(RegisterUserViewModel userfromrequest)
-        {
-           if(ModelState.IsValid)
-            {
+        //[HttpGet]
+        //public IActionResult Register()
+        //{
+        //    return View(Register);
+        //}
+        //[HttpPost]
+        //public IActionResult Register(RegisterUserViewModel userfromrequest)
+        //{
+        //   if(ModelState.IsValid)
+        //    {
 
-            }
-        }
+        //    }
+        //}
     }
 }
---
+++ ProSkills/Controllers/AdminController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProSkills.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+    public class TreeDatas
+    {
+        public string nodeId { get; set; }
+        public string nodeText { get; set; }
+        public string iconCss { get; set; }
+        public bool hasChild { get; set; }
+        public string pid { get; set; }
+    }
+}
