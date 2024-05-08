8fb4e89377a4fbb394bdfc7fc9f71d2947448160
Gamal Saied <gsaied12@gmail.com>
2024-05-08 12:14:52 +0300

    Gamal category

---
+++ ProSkills/Models/ClientSide/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace ProSkills.Models.ClientSide
+{
+    public class Category
+    {
+        
+            public int CCategoryId { get; set; }
+            public string CCategoryName { get; set; }
+            public string CreatedAt{ get; set;}
+            public bool IsDeleted { get; set;}
+    }
+}
