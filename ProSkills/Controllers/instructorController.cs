fbd3cb17dca85cafb1fde1a9afbb956ffeb685a6
Muhammed Ahmed <sadaatvip@gmail.com>
2024-05-08 15:02:30 +0300

    Sadat - instractor

--- ProSkills/Controllers/instructorController.cs
+++ ProSkills/Controllers/instructorController.cs
@@ -1,12 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ProSkills.Interfaces;
+using ProSkills.Models.AdminPanel.Instractor;
+using ProSkills.Models.ClientSide;
+using ProSkills.Repository;
 
 namespace ProSkills.Controllers
 {
     public class InstructorController : Controller
     {
-        public IActionResult Index()
+
+        private IRepository<instructor> _instructorRepository;
+
+        public InstructorController(IRepository<instructor> InstructorRepository)
         {
-            return View();
+            _instructorRepository = InstructorRepository;
         }
+
+        //public IActionResult index()
+        //{
+        //    List<Course> courses = /*_instructorRepository*/.GetAll();
+
+        //    //return View("Showall", instractorList); //View Index ,Model List<Department>
+            
+        //}
     }
 }
