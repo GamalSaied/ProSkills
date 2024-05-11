44e7603bb9332a6dea6e47529fc61a54b2d51a11
Gamal Saied <gsaied12@gmail.com>
2024-05-11 16:28:38 +0300

    ga

--- ProSkills/Models/ITIContext.cs
+++ ProSkills/Models/ITIContext.cs
@@ -1,4 +1,4 @@
-﻿dausing Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using ProSkills.Models.AdminPanel.InstructorManger;
 using ProSkills.Models.ClientSide;
